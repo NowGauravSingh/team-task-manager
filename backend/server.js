75d09216f24216b782f1ff2b24a5ebdb7e8435ef
Gaurav <gaur70202@gmail.com>
2026-05-11 09:44:11 +0530

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://your-vercel-url.vercel.app",
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
